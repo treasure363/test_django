afea4ebea53f785721a9f62db4c34891254e832e
Ankit Choudhury <ankit.choudhury0608@gmail.com>
2022-11-17 20:24:16 +0530

    updates

--- test_django/settings.py
+++ test_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lpaqbfchgmtnrn&@^xbxw^6t22(+ok#x71dz0n=qyloztnpw1j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://treasure363.pythonanywhere.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['treasure363.pythonanywhere.com', '.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
